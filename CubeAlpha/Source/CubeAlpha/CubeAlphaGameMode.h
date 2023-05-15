76b3ac1e432210568eb5487dc2e6ed645520c3c7
Cookie <virgilou92@gmail.com>
2023-05-15 14:51:14 -0400

    test

---
+++ CubeAlpha/Source/CubeAlpha/CubeAlphaGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CubeAlphaGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACubeAlphaGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACubeAlphaGameMode();
+};
+
+
+
